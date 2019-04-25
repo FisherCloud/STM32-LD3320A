de5855cfef54a0a57fabb3479d14003d7f19be59
unknown <jenycc>
2019-04-25 11:06:12 +0800

    删除编译产生的文件

--- RVMDK/Obj/cc936.d
+++
@@ -1,4 +0,0 @@
-.\obj\cc936.o: ..\FATFS\option\cc936.c
-.\obj\cc936.o: ..\FATFS\option\../ff.h
-.\obj\cc936.o: ..\FATFS\option\../integer.h
-.\obj\cc936.o: ..\FATFS\option\../ffconf.h
--- RVMDK/Obj/fatfs_posix.d
+++
@@ -1,8 +0,0 @@
-.\obj\fatfs_posix.o: ..\FATFS\fatfs_posix.c
-.\obj\fatfs_posix.o: D:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\obj\fatfs_posix.o: D:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\obj\fatfs_posix.o: D:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\obj\fatfs_posix.o: ..\FATFS\ff.h
-.\obj\fatfs_posix.o: ..\FATFS\integer.h
-.\obj\fatfs_posix.o: ..\FATFS\ffconf.h
-.\obj\fatfs_posix.o: ..\FATFS\fatfs_posix.h
